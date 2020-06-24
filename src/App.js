6558813cf44dd3711eaa560d8740905eb269d7b6
dacardenasa <diego.cardenasaleg@gmail.com>
2020-06-24 17:43:04 -0500

    Add copyText function to App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.copyText = this.copyText.bind(this);
+
+    this.state = {
+      text: ''
+    }
+
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" onChange={this.copyText} placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.text}</p>
       </div>
     );
   }
+
+  copyText(event){
+    this.setState({
+      text: event.target.value
+    });
+  }
+
 }
 
 export default App;
